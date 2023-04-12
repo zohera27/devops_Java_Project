a0b4cc098582f7d630b8324a820be877f430d65c
zohera27 <shazhar27@outlook.com>
2023-04-12 08:48:25 +0530

    Change in Source COde

--- src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
+++ src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
@@ -1,16 +1,24 @@
 package com.minikube.sample;
-
+import org.springframework.context.ApplicationContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MinikubeSampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext context;
 
+    @Test
+    public void contextLoads() {
+        assertThat(context, notNullValue());
+    }
 }
+
+
